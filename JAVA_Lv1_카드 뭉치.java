517c6cedfb24befbb106d5f6be21d19afb8f7536
21latte <127061330+21latte@users.noreply.github.com>
2023-03-08 10:07:06 +0900

    Create JAVA_Lv1_카드 뭉치.java

---
+++ JAVA_Lv1_카드 뭉치.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int a = 0, b = 0;
+        for(int i = 0; i < goal.length; i++){
+            if(a < cards1.length && cards1[a].equals(goal[i])) a++;
+            else if(b < cards2.length && cards2[b].equals(goal[i])) b++;
+            else return "No";
+        }
+        return "Yes";
+    }
+}
