4cdde8cc05fbd8175b83a5a57f561cef589987d9
21latte <127061330+21latte@users.noreply.github.com>
2023-03-14 22:10:29 +0900

    Create Java_Lv1_월간 코드 챌린지 시즌3_나머지가 1이 되는 수 찾기.java

---
+++ Java_Lv1_월간 코드 챌린지 시즌3_나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        n--;
+        if(n % 2 == 0) return 2;
+        for(int i = 3; i <= n; i += 2){
+            if(n % i == 0) return i;
+        }
+        return 0;
+    }
+}
